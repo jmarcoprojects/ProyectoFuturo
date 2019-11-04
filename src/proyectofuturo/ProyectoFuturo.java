a51ca78a69c4dd0700b73b82d34c3a8876d71e0d
J M <J M@LAPTOP-HKV40MR6>
2019-11-04 12:55:43 +0100

    fin

--- src/proyectofuturo/ProyectoFuturo.java
+++ src/proyectofuturo/ProyectoFuturo.java
@@ -7,7 +7,7 @@ package proyectofuturo;
 
 /**
  *
- * @author J M
+ * @author Juan Antonio Marco
  */
 public class ProyectoFuturo {
 
@@ -20,7 +20,7 @@ public class ProyectoFuturo {
         System.out.println(veredicto(args[1], args[2]));
         
     }
-    static String veredicto(String valor1, String valor2) {
+    private static String veredicto(String valor1, String valor2) {
         if (valor1.equals(valor2)){
             return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
         }
