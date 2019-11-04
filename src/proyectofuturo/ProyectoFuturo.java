3d1205c7928e75b4677414e35205ea12889f529f
J M <J M@LAPTOP-HKV40MR6>
2019-11-04 12:45:12 +0100

    primero

---
+++ src/proyectofuturo/ProyectoFuturo.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofuturo;
+
+/**
+ *
+ * @author J M
+ */
+public class ProyectoFuturo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Estudiante " +args[0] + ":");
+        System.out.println(veredicto(args[1], args[2]));
+        
+    }
+    static String veredicto(String valor1, String valor2) {
+        if (valor1.equals(valor2)){
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+        if (Integer.valueOf(valor1) > Integer.valueOf(valor2)){
+            return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+    }
+   
+    
+}
+ 
